e58c08b7c39eeffb57ea7668ab85ef6956209d59
Samuel8355 <samuelcuomo@gmail.com>
2026-03-05 19:09:23 +0100

    basic documentation

--- matrix/src/matrix/Matrix.java
+++ matrix/src/matrix/Matrix.java
@@ -1,32 +1,79 @@
 package matrix;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * Elke instantie van Matrix stelt een matrix van getallen voor.
+ * @immutable 
+ */
 public class Matrix {
 	
 	/**
-	 * @
+	 * @pre | elements != null
+	 * @pre | rowCount >= 0 && columnCount >=0
+	 * @pre | rowCount*columnCount == elements.length
+	 * @post | getRowCount() == rowCount
+	 * @post | getColumnCount() == columnCount
+	 * @post | Arrays.equals(getRowMajor(), elements)
+	 */
+	Matrix(int rowCount, int columnCount, double[] elements){
+		throw new IllegalArgumentException("not yet implemented");
+	}
+	
+	/**
+	 * @post | result == getRows().length
 	 */
 	public int getRowCount() {
-		throw new illegalArgumentException("not yet implemented");
+		throw new IllegalArgumentException("not yet implemented");
 	}
 	
+	/**
+	 * @post | result == 0 || result == getRows()[0].length
+	 */
 	public int getColumnCount() {
-		throw new illegalArgumentException("not yet implemented");
+		throw new IllegalArgumentException("not yet implemented");
 	}
 	
-	public double getElement() {
-		throw new illegalArgumentException("not yet implemented");
+	/**
+	 * @pre | getRowCount() > 0
+	 * @pre | rowIndex >= 0 && rowIndex < getRowCount()
+	 * @pre | columnIndex >= 0 && columnIndex < getColumnCount()
+	 * @post | result == getRows()[rowIndex][columnIndex]
+	 */
+	public double getElement(int rowIndex, int columnIndex) {
+		throw new IllegalArgumentException("not yet implemented");
 	}
+	
 	
 	public double[] getRowMajor() {
-		throw new illegalArgumentException("not yet implemented");
+		throw new IllegalArgumentException("not yet implemented");
 	}
 	
+	/**
+	 * @post
+	 * | IntStream.range(0, getRowCount()).allMatch(i -> IntStream.range(0,getColumnCount()).allMatch(j -> getRows()[i][j]==result[(j)*getRowCount()+i]))
+	 */
 	public double[] getColumnMajor() {
-		throw new illegalArgumentException("not yet implemented");
+		throw new IllegalArgumentException("not yet implemented");
 	}
 	
-	public double[] getRows() {
-		throw new illegalArgumentException("not yet implemented");
+	/**
+	 * @post | IntStream.range(0, getRowCount()).allMatch(i -> IntStream.range(0,getColumnCount()).allMatch(j -> result[i][j]==getRowMajor()[(i)*getColumnCount()+j]))
+	 * @post | result.length == getRowCount() && result[0].length == getColumnCount()
+	 */
+	public double[][] getRows() {
+		throw new IllegalArgumentException("not yet implemented");
+	}
+	
+	
+	
+	public Matrix scaled() {
+		throw new IllegalArgumentException("not yet implemented");
+	}
+	
+	public Matrix plus() {
+		throw new IllegalArgumentException("not yet implemented");
 	}
 	
 }
--- matrix/src/matrix/MatrixTest.java
+++ matrix/src/matrix/MatrixTest.java
@@ -7,8 +7,30 @@ import org.junit.jupiter.api.Test;
 class MatrixTest {
 
 	@Test
-	void test() {
+	void testConstructor() {
+		Matrix matrix = new Matrix(3,3, new double[] {1,2,3,4,5,6,7,8,9});
+		assertEquals(matrix.getRowCount(), 3);
+		assertEquals(matrix.getColumnCount(), 3);
+		assertEquals(matrix.getRows(), new double[][] {{1,2,3}, {4,5,6}, {7,8,9}});
+		assertEquals(matrix.getRowMajor(), new double[] {1,2,3,4,5,6,7,8,9});
+		assertEquals(matrix.getColumnMajor(), new double[][] {{1,4,7},{2,5,8},{3,6,9}});
+		assertEquals(3, matrix.getElement(0, 2));
+	}
+	
+
+	@Test
+	void testScaled() {
+	}
+	
+	@Test
+	void testPlus() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	void testRepresentationExposure() {
+		fail("Not yet implemented");
+	}
+	
 
 }
