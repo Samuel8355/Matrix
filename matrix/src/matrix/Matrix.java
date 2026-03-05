0d9d891dbee3749ec53d3f9e1735d8d970e0f034
Samuel8355 <samuelcuomo@gmail.com>
2026-03-05 16:53:43 +0100

    Initial commit

---
+++ matrix/src/matrix/Matrix.java
@@ -0,0 +1,32 @@
+package matrix;
+
+public class Matrix {
+	
+	/**
+	 * @
+	 */
+	public int getRowCount() {
+		throw new illegalArgumentException("not yet implemented");
+	}
+	
+	public int getColumnCount() {
+		throw new illegalArgumentException("not yet implemented");
+	}
+	
+	public double getElement() {
+		throw new illegalArgumentException("not yet implemented");
+	}
+	
+	public double[] getRowMajor() {
+		throw new illegalArgumentException("not yet implemented");
+	}
+	
+	public double[] getColumnMajor() {
+		throw new illegalArgumentException("not yet implemented");
+	}
+	
+	public double[] getRows() {
+		throw new illegalArgumentException("not yet implemented");
+	}
+	
+}
