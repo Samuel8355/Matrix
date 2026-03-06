00002f5d19290b5309030f2593b0ba0bec1c38b8
Samuel8355 <samuelcuomo@gmail.com>
2026-03-06 12:48:31 +0100

    new documentation

--- matrix/src/matrix/Matrix.java
+++ matrix/src/matrix/Matrix.java
@@ -4,123 +4,98 @@ import java.util.Arrays;
 import java.util.stream.IntStream;
 
 /**
- * Elke instantie van Matrix stelt een matrix van getallen voor.
+ * Elke instantie van Matrix stelt een matrix van 'double'-waarden voor.
  * @immutable 
  */
 public class Matrix {
 	
 	/**
-	 * @representationObject
-	 * @invar
-	 * | columnCount * rowCount == rowMajor.length
+	 * @creates | result, ...result
+	 * @post | result != null
+	 * @post | result.length > 0
+	 * @post | IntStream.range(0, result.length).allMatch(i -> result[i] != null && result.length >=0)
 	 */
-	double[] rowMajor;
-	int columnCount;
-	int rowCount;
-	
-	/**
-	 * @pre | elements != null
-	 * @pre | rowCount >= 0 && columnCount >=0
-	 * @pre | rowCount*columnCount == elements.length
-	 * @post | getRowCount() == rowCount
-	 * @post | getColumnCount() == columnCount
-	 * @post | Arrays.equals(getRowMajor(), elements)
-	 */
-	Matrix(int rowCount, int columnCount, double[] elements){
-		rowMajor = elements.clone();
-		this.rowCount = rowCount;
-		this.columnCount = columnCount;
-	}
-	
-
-	public int getRowCount() {
-		return rowCount;
-	}
-	
-	public int getColumnCount() {
-		return columnCount;
+	public double[][] getRows(){
+		throw new IllegalArgumentException("not yet implemented");
 	}
 	
 	/**
-	 * @pre | rowIndex >= 0 && rowIndex < getRowCount()
-	 * @pre | columnIndex >= 0 && columnIndex < getColumnCount()
-	 * @creates | result
-	 * @post | result == getRows()[rowIndex][columnIndex]
+	 * @post result == getRows().length
 	 */
-	public double getElement(int rowIndex, int columnIndex) {
-		return getRows()[rowIndex][columnIndex];
+	public int getRowCount(){
+		throw new IllegalArgumentException("not yet implemented");
+	}
+	
+	/**
+	 * @post result == getRows()[0].length
+	 */
+	public int getColCount(){
+		throw new IllegalArgumentException("not yet implemented");
 	}
 	
 	/**
 	 * @creates | result
+	 * @post | result != null
+	 * @post | IntStream.range(0, getRowCount()).allMatch(i -> 
+     * | 			IntStream.range(0, getColCount()).allMatch(j -> result[i*getColCount()+j] == getRows()[i][j]))
 	 */
 	public double[] getRowMajor() {
-		return rowMajor.clone();
+		throw new IllegalArgumentException("not yet implemented");
 	}
 	
 	/**
 	 * @creates | result
-	 * @post
-	 * | IntStream.range(0, getRowCount()).allMatch(i -> IntStream.range(0,getColumnCount()).allMatch(j -> getRows()[i][j]==result[(j)*getRowCount()+i]))
+	 * @post | result != null
+	 * @post | IntStream.range(0, getRowCount()).allMatch(i -> 
+     * | 			IntStream.range(0, getColCount()).allMatch(j -> result[j*getColCount()+i] == getRows()[i][j]))
 	 */
-	public double[] getColumnMajor() {
-		double[] result = new double[rowMajor.length];
-		for (int i=0; i<rowCount; i++) {
-			for (int j=0;j<columnCount;j++) {
-				result[j*rowCount+i] = rowMajor[i*columnCount+j];
-			}
-		}
-		return result;
+	public double[] getColMajor() {
+		throw new IllegalArgumentException("not yet implemented");
 	}
 	
 	/**
-	 * @creates | result
-	 * @post | IntStream.range(0, getRowCount()).allMatch(i -> IntStream.range(0,getColumnCount()).allMatch(j -> result[i][j]==getRowMajor()[(i)*getColumnCount()+j]))
-	 * @post | result.length == getRowCount() && result[0].length == getColumnCount()
+	 * @pre | row < getRowCount() && row >= 0
+	 * @pre | col < getColCount() && col >= 0
+	 * @post | result == getRows()[row][col]
 	 */
-	public double[][] getRows() {
-		double[][] result = new double [rowCount][columnCount];
-		for (int i=0; i<rowCount; i++) {
-			for (int j=0;j<columnCount;j++) {
-				result[i][j] = rowMajor[i*columnCount+j];
-			}
-		}
-		return result;
+	public double getElement(int row, int col) {
+		throw new IllegalArgumentException("not yet implemented");
+	}
+	
+	/** 
+	 *  @pre | rowMajor != null
+	 *  @pre | rowCount * colCount == rowMajor.length
+	 *  @pre | rowCount >= 0 && colCount >=0
+	 *	@inspects | rowMajor
+	 *	@post | getRowCount() == rowCount
+	 *	@post | getColCount() == colCount
+	 *	@post | getRowMajor() == rowMajor
+	 */
+	Matrix(int rowCount, int colCount, double[] rowMajor){
+		throw new IllegalArgumentException("not yet implemented");
 	}
 	
 	
 	/**
-	 * @creates | result
-	 * @inspects | this
-	 * @post | Arrays.equals(getRowMajor(), old(getRowMajor()))
-	 * @post | getRowCount() == old(getRowCount()) && getColumnCount() == old(getColumnCount())
-	 * @post | result.getRowMajor().length == getRowMajor().length
-	 * @post | IntStream.range(0, getRowMajor().length).allMatch(i -> result.getRowMajor()[i] == scalar*getRowMajor()[i])
+	 * @post | result != null
+	 * @post | result.getRowCount() == getRowCount()
+	 * @post | result.getColCount() == getColCount()
+	 * @post | IntStream.range(0,getRowMajor().length).allMatch(i -> getRowMajor()[i]*scalar == result.getRowMajor()[i])
 	 */
-	
-	public Matrix scaled(double scalar) {
-		double[] newRowMajor = rowMajor.clone();
-		for (int i=0;i<rowMajor.length;i++) {
-			newRowMajor[i] = newRowMajor[i]*scalar;
-		}
-		Matrix result = new Matrix(rowCount, columnCount, newRowMajor);
-		return result;
+	Matrix scaled(double scalar) {
+		throw new IllegalArgumentException("not yet implemented");
 	}
 	
 	/**
-	 * @creates | result
-	 * @inspects | this, matrix
-	 * @post | Arrays.equals(getRowMajor(), old(getRowMajor()))
-	 * @post | getRowCount() == old(getRowCount()) && getColumnCount() == old(getColumnCount())
-	 * @post | result.getRowMajor().length == getRowMajor().length
-	 * @post | IntStream.range(0, getRowMajor().length).allMatch(i -> result.getRowMajor()[i] == matrix.getRowMajor()[i]+ getRowMajor()[i])
+	 * @pre | other != null
+	 * @pre | other.getRowCount() == getRowCount()
+	 * @pre | other.getColCount() == getColCount()
+	 * @post | result != null
+	 * @post | result.getRowCount() == getRowCount()
+	 * @post | result.getColCount() == getColCount()
+	 * @post | IntStream.range(0,getRowMajor().length).allMatch(i -> getRowMajor()[i]*+ other.getRowMajor()[i] == result.getRowMajor()[i])
 	 */
-	public Matrix plus(Matrix matrix) {
-		double[] newRowMajor = new double[rowMajor.length];
-		for (int i = 0;i<rowMajor.length;i++) {
-			newRowMajor[i] = rowMajor[i] + matrix.getRowMajor()[i];
-		}
-		return new Matrix(rowCount, columnCount, newRowMajor);
-}
-	
+	Matrix plus(Matrix other) {
+		throw new IllegalArgumentException("not yet implemented");
+	}
 }
--- matrix/src/matrix/MatrixTest.java
+++ matrix/src/matrix/MatrixTest.java
@@ -10,10 +10,10 @@ class MatrixTest {
 	void testConstructor() {
 		Matrix matrix = new Matrix(3,3, new double[] {1,2,3,4,5,6,7,8,9});
 		assertEquals(matrix.getRowCount(), 3);
-		assertEquals(matrix.getColumnCount(), 3);
+		assertEquals(matrix.getColCount(), 3);
 		assertArrayEquals(matrix.getRows(), new double[][] {{1,2,3}, {4,5,6}, {7,8,9}});
 		assertArrayEquals(matrix.getRowMajor(), new double[] {1,2,3,4,5,6,7,8,9});
-		assertArrayEquals(matrix.getColumnMajor(), new double[] {1,4,7,2,5,8,3,6,9});
+		assertArrayEquals(matrix.getColMajor(), new double[] {1,4,7,2,5,8,3,6,9});
 		assertEquals(3, matrix.getElement(0, 2));
 	}
 	
@@ -22,46 +22,46 @@ class MatrixTest {
 	void testScaled() {
 		Matrix matrix1 = new Matrix(2,2, new double[] {1,2,3,4});
 		assertEquals(matrix1.getRowCount(), 2);
-		assertEquals(matrix1.getColumnCount(), 2);
+		assertEquals(matrix1.getColCount(), 2);
 		assertArrayEquals(matrix1.getRows(), new double[][] {{1,2}, {3,4}});
 		assertArrayEquals(matrix1.getRowMajor(), new double[] {1,2,3,4});
-		assertArrayEquals(matrix1.getColumnMajor(), new double[] {1,3,2,4});
+		assertArrayEquals(matrix1.getColMajor(), new double[] {1,3,2,4});
 		assertEquals(4, matrix1.getElement(1, 1));
 		
 		double scalar = 2;
 		
 		Matrix result = matrix1.scaled(scalar);
 		assertEquals(result.getRowCount(), 2);
-		assertEquals(result.getColumnCount(), 2);
+		assertEquals(result.getColCount(), 2);
 		assertArrayEquals(result.getRows(), new double[][] {{2,4},{6,8}});
 		assertArrayEquals(result.getRowMajor(), new double[] {2,4,6,8});
-		assertArrayEquals(result.getColumnMajor(), new double[] {2,6,4,8});
+		assertArrayEquals(result.getColMajor(), new double[] {2,6,4,8});
 	}
 	
 	@Test
 	void testPlus() {
 		Matrix matrix1 = new Matrix(2,2, new double[] {1,2,3,4});
 		assertEquals(matrix1.getRowCount(), 2);
-		assertEquals(matrix1.getColumnCount(), 2);
+		assertEquals(matrix1.getColCount(), 2);
 		assertArrayEquals(matrix1.getRows(), new double[][] {{1,2}, {3,4}});
 		assertArrayEquals(matrix1.getRowMajor(), new double[] {1,2,3,4});
-		assertArrayEquals(matrix1.getColumnMajor(), new double[] {1,3,2,4});
+		assertArrayEquals(matrix1.getColMajor(), new double[] {1,3,2,4});
 		assertEquals(4, matrix1.getElement(1, 1));
 		
 		Matrix matrix2 = new Matrix(2,2, new double[] {2,3,4,5});
 		assertEquals(matrix2.getRowCount(), 2);
-		assertEquals(matrix2.getColumnCount(), 2);
+		assertEquals(matrix2.getColCount(), 2);
 		assertArrayEquals(matrix2.getRows(), new double[][] {{2,3},{4,5}});
 		assertArrayEquals(matrix2.getRowMajor(), new double[] {2,3,4,5});
-		assertArrayEquals(matrix2.getColumnMajor(), new double[] {2,4,3,5});
+		assertArrayEquals(matrix2.getColMajor(), new double[] {2,4,3,5});
 		assertEquals(2, matrix2.getElement(0, 0));
 		
 		Matrix result = matrix1.plus(matrix2);
 		assertEquals(result.getRowCount(), 2);
-		assertEquals(result.getColumnCount(), 2);
+		assertEquals(result.getColCount(), 2);
 		assertArrayEquals(result.getRows(), new double[][] {{3,5},{7,9}});
 		assertArrayEquals(result.getRowMajor(), new double[] {3,5,7,9});
-		assertArrayEquals(result.getColumnMajor(), new double[] {3,7,5,9});
+		assertArrayEquals(result.getColMajor(), new double[] {3,7,5,9});
 	}
 	
 	@Test
@@ -70,54 +70,54 @@ class MatrixTest {
 		double[] list1 = new double[] {1,2,3,4,5,6,7,8,9};
 		Matrix matrix = new Matrix(3,3, list1);
 		assertEquals(matrix.getRowCount(), 3);
-		assertEquals(matrix.getColumnCount(), 3);
+		assertEquals(matrix.getColCount(), 3);
 		assertArrayEquals(matrix.getRows(), new double[][] {{1,2,3}, {4,5,6}, {7,8,9}});
 		assertArrayEquals(matrix.getRowMajor(), new double[] {1,2,3,4,5,6,7,8,9});
-		assertArrayEquals(matrix.getColumnMajor(), new double[] {1,4,7,2,5,8,3,6,9});
+		assertArrayEquals(matrix.getColMajor(), new double[] {1,4,7,2,5,8,3,6,9});
 		assertEquals(3, matrix.getElement(0, 2));
 		
 		list1[0]= 0;
 		assertEquals(matrix.getRowCount(), 3);
-		assertEquals(matrix.getColumnCount(), 3);
+		assertEquals(matrix.getColCount(), 3);
 		assertArrayEquals(matrix.getRows(), new double[][] {{1,2,3}, {4,5,6}, {7,8,9}});
 		assertArrayEquals(matrix.getRowMajor(), new double[] {1,2,3,4,5,6,7,8,9});
-		assertArrayEquals(matrix.getColumnMajor(), new double[] {1,4,7,2,5,8,3,6,9});
+		assertArrayEquals(matrix.getColMajor(), new double[] {1,4,7,2,5,8,3,6,9});
 		assertEquals(3, matrix.getElement(0, 2));
 		
 		double[][] list2 = matrix.getRows();
 		list2[0][0] = 100;
 		assertEquals(matrix.getRowCount(), 3);
-		assertEquals(matrix.getColumnCount(), 3);
+		assertEquals(matrix.getColCount(), 3);
 		assertArrayEquals(matrix.getRows(), new double[][] {{1,2,3}, {4,5,6}, {7,8,9}});
 		assertArrayEquals(matrix.getRowMajor(), new double[] {1,2,3,4,5,6,7,8,9});
-		assertArrayEquals(matrix.getColumnMajor(), new double[] {1,4,7,2,5,8,3,6,9});
+		assertArrayEquals(matrix.getColMajor(), new double[] {1,4,7,2,5,8,3,6,9});
 		assertEquals(3, matrix.getElement(0, 2));
 		
-		double[] list3 = matrix.getColumnMajor();
+		double[] list3 = matrix.getColMajor();
 		list3[0] = 100;
 		assertEquals(matrix.getRowCount(), 3);
-		assertEquals(matrix.getColumnCount(), 3);
+		assertEquals(matrix.getColCount(), 3);
 		assertArrayEquals(matrix.getRows(), new double[][] {{1,2,3}, {4,5,6}, {7,8,9}});
 		assertArrayEquals(matrix.getRowMajor(), new double[] {1,2,3,4,5,6,7,8,9});
-		assertArrayEquals(matrix.getColumnMajor(), new double[] {1,4,7,2,5,8,3,6,9});
+		assertArrayEquals(matrix.getColMajor(), new double[] {1,4,7,2,5,8,3,6,9});
 		assertEquals(3, matrix.getElement(0, 2));
 		
-		double[] list4 = matrix.getColumnMajor();
+		double[] list4 = matrix.getColMajor();
 		list4[0] = 100;
 		assertEquals(matrix.getRowCount(), 3); 
-		assertEquals(matrix.getColumnCount(), 3);
+		assertEquals(matrix.getColCount(), 3);
 		assertArrayEquals(matrix.getRows(), new double[][] {{1,2,3}, {4,5,6}, {7,8,9}});
 		assertArrayEquals(matrix.getRowMajor(), new double[] {1,2,3,4,5,6,7,8,9});
-		assertArrayEquals(matrix.getColumnMajor(), new double[] {1,4,7,2,5,8,3,6,9});
+		assertArrayEquals(matrix.getColMajor(), new double[] {1,4,7,2,5,8,3,6,9});
 		assertEquals(3, matrix.getElement(0, 2));
 		
 		double[] list5 = matrix.getRowMajor();
 		list4[2] = 100;
 		assertEquals(matrix.getRowCount(), 3); 
-		assertEquals(matrix.getColumnCount(), 3);
+		assertEquals(matrix.getColCount(), 3);
 		assertArrayEquals(matrix.getRows(), new double[][] {{1,2,3}, {4,5,6}, {7,8,9}});
 		assertArrayEquals(matrix.getRowMajor(), new double[] {1,2,3,4,5,6,7,8,9});
-		assertArrayEquals(matrix.getColumnMajor(), new double[] {1,4,7,2,5,8,3,6,9});
+		assertArrayEquals(matrix.getColMajor(), new double[] {1,4,7,2,5,8,3,6,9});
 		assertEquals(3, matrix.getElement(0, 2));
 				
 	}
